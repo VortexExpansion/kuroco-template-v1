2ef3d0facd2ce573b6b58954aa97deb178790dfb
HisashiHasebe <hhasebe@diverta.co.jp>
2022-11-24 17:12:14 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <header class="p-header">
+      <div class="p-header__inner l-header">
+        <h1 class="p-header__logo"><a href="https://rcms-template-v1.r-cms.jp/"><img
+              src="https://rcms-template-v1.r-cms.jp/files/user/images/logo.svg?v=1516164532" alt="RCMS テンプレート"></a></h1>
+        <div class="icon-animation type-1" data-js="nav-menu">
+          <span class="top"></span>
+          <span class="middle"></span>
+          <span class="bottom"></span>
+        </div>
+        <ul class="p-header__menu">
+          <li class="p-header__menu__item active"><a href="https://rcms-template-v1.r-cms.jp/">トップ</a></li>
+          <li class="p-header__menu__item"><a href="https://rcms-template-v1.r-cms.jp/news/">ニュース</a></li>
+          <li class="p-header__menu__item"><a href="https://rcms-template-v1.r-cms.jp/service/">事業内容</a></li>
+          <li class="p-header__menu__item"><a href="https://rcms-template-v1.r-cms.jp/company/">企業情報</a></li>
+          <li class="p-header__menu__item"><a href="https://rcms-template-v1.r-cms.jp/contact/">お問い合わせ</a></li>
+        </ul>
+      </div>
+    </header>
+
+    <Nuxt />
+    <footer class="p-footer">
+      <div class="p-footer__inner l-footer">
+        <div class="p-footer__menu">
+          <div class="p-footer__menu__head">
+            <a href="https://rcms-template-v1.r-cms.jp/">
+              <img src="/files/user/images/logo_wh.svg?v=1516164532" alt="RCMS テンプレート">
+            </a>
+          </div>
+          <div class="p-footer__menu__body">
+            <ul>
+              <li><a href="https://rcms-template-v1.r-cms.jp/"><i class="p-footer__menu__icon fas fa-angle-right"
+                    aria-hidden="true"></i>トップ</a></li>
+              <li><a href="https://rcms-template-v1.r-cms.jp/privacy/"><i
+                    class="p-footer__menu__icon fas fa-angle-right" aria-hidden="true"></i>プライバシーポリシー</a></li>
+              <li><a href="https://rcms-template-v1.r-cms.jp/sitemap/"><i
+                    class="p-footer__menu__icon fas fa-angle-right" aria-hidden="true"></i>サイトマップ</a></li>
+            </ul>
+            <ul>
+              <li><a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-square fa-2x"
+                    aria-hidden="true"></i></a></li>
+              <li><a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter-square fa-2x"
+                    aria-hidden="true"></i></a></li>
+              <li><a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram fa-2x"
+                    aria-hidden="true"></i></a></li>
+              <li><a href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-youtube fa-2x"
+                    aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="p-footer__copyright">Copyright(c) 2018 * All Rights Reserved. managed by <a
+          href="https://www.r-cms.jp/" rel="nofollow">RCMS</a></div>
+    </footer>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <section class="l-container--contents">
+      <div class="c-slider flexslider" data-js="flexslider">
+
+        <div class="flex-viewport" style="overflow: hidden; position: relative;">
+          <ul class="c-slider__inner slides"
+            style="width: 800%; transition-duration: 0s; transform: translate3d(-803.333px, 0px, 0px);">
+            <li class="c-slider__item clone" aria-hidden="true"
+              style="width: 803.333px; margin-right: 0px; float: left; display: block;">
+              <a rel="noopener" href="https://rcms-template-v1.r-cms.jp/news/" target="_blank">
+                <picture>
+                  <source media="(max-width: 768px)" srcset="/files/topics/30_ext_02_0.png?v=1516778702"> <img
+                    src="/files/topics/30_ext_01_0.png?v=1516941623" alt="" draggable="false">
+                </picture>
+              </a>
+            </li>
+            <li class="c-slider__item flex-active-slide"
+              style="width: 803.333px; margin-right: 0px; float: left; display: block;" data-thumb-alt="">
+              <picture>
+                <source media="(max-width: 768px)" srcset="/files/topics/29_ext_02_0.png?v=1516778702"> <img
+                  src="/files/topics/29_ext_01_0.png?v=1516941605" alt="" draggable="false">
+              </picture>
+            </li>
+            <li class="c-slider__item" data-thumb-alt=""
+              style="width: 803.333px; margin-right: 0px; float: left; display: block;">
+              <a rel="noopener" href="https://rcms-template-v1.r-cms.jp/news/" target="_blank">
+                <picture>
+                  <source media="(max-width: 768px)" srcset="/files/topics/30_ext_02_0.png?v=1516778702"> <img
+                    src="/files/topics/30_ext_01_0.png?v=1516941623" alt="" draggable="false">
+                </picture>
+              </a>
+            </li>
+            <li class="c-slider__item clone" style="width: 803.333px; margin-right: 0px; float: left; display: block;"
+              aria-hidden="true">
+              <picture>
+                <source media="(max-width: 768px)" srcset="/files/topics/29_ext_02_0.png?v=1516778702"> <img
+                  src="/files/topics/29_ext_01_0.png?v=1516941605" alt="" draggable="false">
+              </picture>
+            </li>
+          </ul>
+        </div>
+        <ol class="flex-control-nav flex-control-paging">
+          <li><a href="#" class="flex-active">1</a></li>
+          <li><a href="#" class="">2</a></li>
+        </ol>
+      </div>
+    </section>
+
+    <section class="l-container--middle l-container--contents t-article-list p-top-topics__list">
+      <ul class="c-topics__list">
+        <li class="c-topics__item">
+          <time class="c-topics__date" datetime="2018-02-01">2018.02.01</time>
+          <div class="c-topics__label">
+            カテゴリー①
+          </div>
+          <div class="c-topics__title">
+            <a href="https://rcms-template-v1.r-cms.jp/news/detail/id=13">このテンプレートをご利用いただくにあたって</a>
+          </div>
+        </li>
+        <li class="c-topics__item">
+          <time class="c-topics__date" datetime="2018-01-24">2018.01.24</time>
+          <div class="c-topics__label">
+            カテゴリー①
+          </div>
+          <div class="c-topics__title">
+            <a href="https://rcms-template-v1.r-cms.jp/news/detail/id=28">HTMLパーツの使い方</a>
+          </div>
+        </li>
+        <li class="c-topics__item">
+          <time class="c-topics__date" datetime="2018-01-10">2018.01.10</time>
+          <div class="c-topics__label">
+            カテゴリー②
+          </div>
+          <div class="c-topics__title">
+            <a href="https://rcms-template-v1.r-cms.jp/news/detail/id=6">テスト記事です。テスト記事です。テスト記事です。テスト記事です。</a>
+          </div>
+        </li>
+      </ul>
+      <div class="u-flex-horizon-center">
+        <a class="c-button icon-arrow-right" href="https://rcms-template-v1.r-cms.jp/news/">一覧へ</a>
+      </div>
+    </section>
+
+    <section class="l-container--middle l-container--contents t-article-list">
+      <ul class="c-topics__card__list">
+        <li class="c-topics__card__item"><a class="c-topics__card__link"
+            href="https://rcms-template-v1.r-cms.jp/service/#service-01">
+            <div class="c-topics__card__picture"><img alt="dummy picture"
+                src="https://rcms-template-v1.r-cms.jp/files/user/images/600x400.png?v=1513757334"></div>
+
+            <div class="c-topics__card__contents">
+              <div class="c-topics__title">テキストが入ります。テキストが入ります。</div>
+            </div>
+          </a></li>
+        <li class="c-topics__card__item"><a class="c-topics__card__link"
+            href="https://rcms-template-v1.r-cms.jp/service/#service-02">
+            <div class="c-topics__card__picture"><img alt="dummy picture"
+                src="https://rcms-template-v1.r-cms.jp/files/user/images/600x400.png?v=1513757334"></div>
+
+            <div class="c-topics__card__contents">
+              <div class="c-topics__title">テキストが入ります。テキストが入ります。</div>
+            </div>
+          </a></li>
+        <li class="c-topics__card__item"><a class="c-topics__card__link"
+            href="https://rcms-template-v1.r-cms.jp/service/#service-03">
+            <div class="c-topics__card__picture"><img alt="dummy picture"
+                src="https://rcms-template-v1.r-cms.jp/files/user/images/600x400.png?v=1513757334"></div>
+
+            <div class="c-topics__card__contents">
+              <div class="c-topics__title">テキストが入ります。テキストが入ります。</div>
+            </div>
+          </a></li>
+      </ul>
+
+      <div class="u-flex-horizon-center"><a class="c-button icon-arrow-right"
+          href="https://rcms-template-v1.r-cms.jp/service/">一覧へ</a></div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
