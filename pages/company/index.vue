11ae7ace3b772bb4fd6fb30796ce70226ea3048d
HisashiHasebe <hhasebe@diverta.co.jp>
2022-12-02 10:50:13 +0800

    update:ページ追加

---
+++ pages/company/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="l-container--wrap">
+    <nav class="l-breadcrumb is-pc">
+      <div class="l-container--middle">
+        <ul>
+          <li><nuxt-link to="/">トップ</nuxt-link></li>
+          <li>ご挨拶</li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="l-container--middle l-container--contents">
+      <article>
+        <div class="topics_contents">
+          <div v-html="response.details.contents"></div>
+          <div class="clr"></div>
+        </div>
+      </article>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    return {
+      response: await $axios.$get(`/rcms-api/1/content/details/company`),
+    };
+  },
+};
+</script>
