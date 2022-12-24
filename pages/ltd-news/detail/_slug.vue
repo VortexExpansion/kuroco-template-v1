6cfbdcd7dcc5c5ae61dac80b5250dfb52d7b6b07
HisashiHasebe <hhasebe@diverta.co.jp>
2022-12-25 00:54:50 +0800

    update:限定記事の見せ方更新

--- pages/ltd-news/detail/_slug.vue
+++ pages/ltd-news/detail/_slug.vue
@@ -5,30 +5,31 @@
         <ul>
           <li><nuxt-link to="/">トップ</nuxt-link></li>
           <li><nuxt-link to="/news/">ニュース</nuxt-link></li>
-          <li>{{ response.details.subject }}</li>
+          <li></li>
         </ul>
       </div>
     </nav>
 
-    <div class="l-container--middle l-container--contents">
-
+    <div v-if="response != null" class="l-container--middle l-container--contents">
       <div class="l-container--main">
         <article class="c-article p-newsDetail">
           <div class="p-newsDetail__head">
             <time class="p-newsDetail__head__date" :datetime=response.details.ymd>{{ response.details.ymd }}</time>
-            <div class="p-newsDetail__head__label">{{response.details.contents_type_nm}}</div>
+            <div class="p-newsDetail__head__label">{{ response.details.contents_type_nm }}</div>
             <h1 class="c-heading--lv1 p-newsDetail__head__heading">{{ response.details.subject }}</h1>
           </div>
-
           <div v-html="response.details.contents"></div>
-
           <div class="p-newsDetail__foot">
-            <nuxt-link to="/ltd-news/"
-              class="c-button--return icon-arrow-left">限定記事一覧へ戻る</nuxt-link>
+            <nuxt-link to="/ltd-news/" class="c-button--return icon-arrow-left">限定記事一覧へ戻る</nuxt-link>
           </div>
         </article>
-
       </div>
+    </div>
+    <div v-else-if="profileRes.member_id != null">
+      プレミアム会員限定記事です。
+    </div>
+    <div v-else>
+      ログインしてください。
     </div>
 
   </div>
@@ -37,9 +38,35 @@
 <script>
 export default {
   async asyncData({ $axios, params }) {
-    return {
-      response: await $axios.$get(`/rcms-api/1/ltd-news/details/${params.slug}`),
-    };
+    const profileRes = await $axios.$get('/rcms-api/1/profile')
+    if (profileRes.member_id != null) {
+      if ('105' in profileRes.group_ids) {
+        return {
+          response: await $axios.$get(`/rcms-api/1/ltd-news/details/105/${params.slug}`),
+          profileRes: profileRes
+        }
+      }
+      else {
+        try {
+          const response = await $axios.$get(`/rcms-api/1/ltd-news/details/104/${params.slug}`)
+          return { response, profileRes }
+        }
+        catch (error) {
+          console.log(error.message);
+          return { error, profileRes }
+        }
+      }
+    }
+    else {
+      try {
+        const response = await $axios.$get(`/rcms-api/1/ltd-news/details/public/${params.slug}`)
+        return { response, profileRes }
+      }
+      catch (error) {
+        console.log(error.message);
+        return { error, profileRes }
+      }
+    }
   },
 };
 </script>
