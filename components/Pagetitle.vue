adcf9d48c82f79236adf8fe14f09b4f0eee1336c
mimori <mimori@diverta.co.jp>
2023-07-26 22:47:26 +0900

    add: sass file

---
+++ components/Pagetitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="subject" class="c-page-title">
+    <div class="l-container--large">
+      <h1 class="c-page-title__heading">{{ subject }}</h1>
+      <time v-if="ymd" :datetime="ymd" class="c-page-title__date">{{ ymd }}</time>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subject: {
+      type: String,
+      required: false,
+    },
+    ymd: {
+      type: Date,
+      required: false,
+    },
+  },
+};
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'assets/parts.css',
+    '@/assets/scss/style.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
