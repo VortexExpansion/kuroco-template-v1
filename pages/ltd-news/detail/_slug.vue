419f41274af6f6b18db1798507f5cb2d63d35888
mimori <mimori@diverta.co.jp>
2023-08-24 20:13:52 +0900

    fix: ltd-detail page design ajustment

--- pages/ltd-news/detail/_slug.vue
+++ pages/ltd-news/detail/_slug.vue
@@ -1,14 +1,73 @@
 <template>
   <div>
-    <UiNavLink v-if="details != null" :path="path" :subject="details.subject" />
-    <UiPagetitle v-if="details != null" :subject="details.group_nm" :subheading="subheading" />
-    <div class="l-container--large l-container--contents">
-      <ContentDetailBody v-if="details != null" :details="details" :button="button" />
-      <div v-else-if="profileRes.member_id != null">
-        プレミアム会員会員限定コンテンツです。
+    <template v-if="details != null">
+      <UiNavLink :path="path" :subject="details.subject" />
+      <UiPagetitle :subject="subject" :subheading="subheading" />
+      <div class="l-container--large l-container--contents">
+        <ContentDetailBody :details="details" :button="button" />
       </div>
-      <div v-else>ログインしてください。</div>
-    </div>
+    </template>
+    <template v-else>
+      <UiNavLink :path="path" :subject="subject" />
+      <UiPagetitle :subject="subject" :subheading="subheading" />
+      <div class="l-container--small l-container--contents">
+        <div class="c-alert c-alert--error">
+          <div class="u-display-flex">
+            <div class="c-alert__icon u-display-flex-shrink-0">
+              <svg
+                width="26"
+                height="26"
+                viewBox="0 0 26 26"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="u-display-block"
+              >
+                <circle cx="13" cy="13" r="13" fill="var(--color-error)" />
+                <path
+                  d="M13 8.33331V13"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M13 17.6667H13.01"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <div v-if="profileRes.member_id != null">
+              <p class="c-alert__heading">
+                プレミアム会員会員限定コンテンツです。
+              </p>
+              <p class="c-alert__message">
+                マイページよりプレミアム会員にアップデートしてご覧ください。
+              </p>
+              <NuxtLink to="/mypage" class="c-button u-pa-15"
+                >マイページ</NuxtLink
+              >
+            </div>
+            <div v-else>
+              <p class="c-alert__heading">
+                会員会員限定コンテンツです。
+              </p>
+              <p class="c-alert__message">
+                会員登録またはログインしてご覧ください。
+              </p>
+              <NuxtLink to="/login/regist" class="c-button u-pa-15"
+                >会員登録</NuxtLink
+              >
+              <NuxtLink to="/login" class="c-button u-pa-15 u-ml-10"
+                >ログイン</NuxtLink
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -18,6 +77,7 @@ export default {
     return {
       path: [{ label: "会員限定コンテンツ", to: "/ltd-news/" }],
       button: [{ label: "会員限定コンテンツ一覧へ戻る", to: "/ltd-news/" }],
+      subject: "会員限定コンテンツ",
       subheading: "For Members",
     };
   },
